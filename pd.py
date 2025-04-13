84e733d9bdc5d74861e115501c8b84f560136e18
AndrewMarinf <andrew.marin@bk.ru>
2025-04-13 19:36:16 +0300

    добавил строчки

--- pd.py
+++ pd.py
@@ -4,3 +4,6 @@ from pyspark import SparkContext
 df = pd.read_csv('IHME-GBD_2019_DATA-ff08d9bc-1.csv',delimiter=',',encoding='utf-8')
 a = 1 
 b = 2 
+df = pd.read_csv('IHME-GBD_2019_DATA-ff08d9bc-1.csv',delimiter=',',encoding='utf-8')
+a = 1 
+b = 2 
