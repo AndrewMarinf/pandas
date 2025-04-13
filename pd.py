49df149b8a226a82bc7115136e7d506e0b9db7fb
AndrewMarinf <andrew.marin@bk.ru>
2025-04-13 19:41:38 +0300

    добавил

--- pd.py
+++ pd.py
@@ -1,3 +1,13 @@
 import pandas as pd
 from pyspark import SparkContext
 
+import pandas as pd
+from pyspark import SparkContext
+import pandas as pd
+from pyspark import SparkContext
+import pandas as pd
+from pyspark import SparkContext
+import pandas as pd
+from pyspark import SparkContext
+import pandas as pd
+from pyspark import SparkContext
